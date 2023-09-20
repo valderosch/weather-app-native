3cd60a2b688218b21bdf2546901549565da4cca9
valderosch <phonelix96@gmail.com>
2023-09-20 10:05:49 +0300

    reorganize
    organized folder

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Weather from './src';
 
--- constants.js
+++ constants.js
@@ -1,8 +1,9 @@
+import React from "react";
 
 export const COLOR = {
     /* App colors */
     primary: "#FFE142", // Primary color - yellow
-    secondary: "#D9F1F9", // BG Color
+    secondary: "#D9F1F9", // BackGround Color
     uactive: "#888888", // Muted color
     white: "#FFFFFF", // Just white 
     black: "#000000", // Just black
@@ -15,4 +16,28 @@ export const COLOR = {
     halfblacked: 'rgba(0, 0, 0, 0.6)', // black mid-opacity color
     halfwhited: 'rgba(255, 255, 255, 0.5)', // white half-opacity
 }
+
+export const FONTSIZE = {
+    /* FONT Sizes
+       Small */ 
+    fontMicro: 12,
+    fontSmall: 15,
+    /* Medium */
+    fontSmallLarge: 16,
+    fontUnderMi: 18,
+    /* Large */
+    fontMediumBig: 22,
+    fontLarge: 23,
+    fontLarger: 25,
+}
+
+export const FONTWEIGHT = {
+    /* FONT Weight */
+    light: '100', // ultra thin
+    light: '200', // thin regular
+    medium: '500', // medium text
+    bold: '700', // bold 
+    bolder: '900', // black ultra
+}
+
 
